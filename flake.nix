a6d450c47b80f6878d065abb8c468a94c3dc2fd1
Ulrik Strid <ulrik.strid@outlook.com>
2022-04-11 11:19:17 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -3,9 +3,15 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, flake-utils }: ({
-    overlay = final: prev: (import ./default.nix) final prev;
+  outputs = { self, nixpkgs, flake-utils }: 
+  let overlay = final: prev: (import ./default.nix) final prev; in
+  ({
+    inherit overlay;  
   } // flake-utils.lib.eachDefaultSystem (system: rec {
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ overlay ];
+    };
     packages = import ./boot.nix {
       inherit system;
       config = {
@@ -15,6 +21,6 @@
     legacyPackages = self.packages."${system}";
     makePkgs = attrs: import ./boot.nix attrs;
 
-    hydraJobs = import ./hydra.nix { pkgs = packages; };
+    hydraJobs = import ./hydra.nix { inherit pkgs; };
   }));
 }
